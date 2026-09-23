0b91035c1d67a68540a2144c9aa496e2439631c8
anushkavarun18 <anushkavarun1809@gmail.com>
2026-09-24 00:33:24 +0530

    Add files via upload

---
+++ 10th que convert sec to hours.py
@@ -0,0 +1,10 @@
+total_seconds = int(input("Enter total seconds: "))
+
+hours = total_seconds // 3600
+remaining = total_seconds % 3600
+minutes = remaining // 60
+seconds = remaining % 60
+
+print("Hours =", hours)
+print("Minutes =", minutes)
+print("Seconds =", seconds)
